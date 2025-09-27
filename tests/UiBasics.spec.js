0c95b2a2fc20cfb88bae54a5beaeb71f248bc5a5
SHasan59 <121329046+SHasan59@users.noreply.github.com>
2025-09-26 21:37:58 -0400

    init

---
+++ tests/UiBasics.spec.js
@@ -0,0 +1,7 @@
+const { test } = require("@playwright/test"); //declare to start tests
+
+test("First Playwright test", async () => {
+  // function() same as ()=>
+  //js is async (doesn't go in order like java)
+  // need to put async on function for await
+});
