ef0c1b7acf302f10920a4f7bb6efe9ccc4cf1679
SHasan59 <hasan.samith@gmail.com>
2025-09-26 21:58:55 -0400

    Added Browser context

--- tests/UiBasics.spec.js
+++ tests/UiBasics.spec.js
@@ -1,7 +1,22 @@
 const { test } = require("@playwright/test"); //declare to start tests
 
-test("First Playwright test", async () => {
+test("Browser Context test", async ({browser, page}) => {
   // function() same as ()=>
   //js is async (doesn't go in order like java)
   // need to put async on function for await
+
+   //opening new browser tab
+   //const context = await browser.newContext();
+  // const page = await context.newPage();
+  // can use page in function decleration (if you don't have cookies)
+
+   await  page.goto("https://google.com");
+
+});
+
+
+test("Page Playwright test", async ({page}) => {
+ 
+   await  page.goto("https://google.com");
+
 });
