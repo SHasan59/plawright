4bbd2dd40ab566d4d5a393293a1def6a01da4e7e
SHasan59 <hasan.samith@gmail.com>
2025-09-29 22:09:28 -0400

    Created config file & added first test

--- tests/UiBasics.spec.js
+++ tests/UiBasics.spec.js
@@ -1,22 +1,20 @@
 const { test } = require("@playwright/test"); //declare to start tests
 
-test("Browser Context test", async ({browser, page}) => {
+test.only("Browser Context test", async ({ browser, page }) => {
   // function() same as ()=>
   //js is async (doesn't go in order like java)
   // need to put async on function for await
 
-   //opening new browser tab
-   //const context = await browser.newContext();
+  //opening new browser tab
+  //const context = await browser.newContext();
   // const page = await context.newPage();
   // can use page in function decleration (if you don't have cookies)
 
-   await  page.goto("https://google.com");
-
+  await page.goto(
+    "https://www.google.com/search?q=f1&rlz=1C1ONGR_enUS999US999&oq=f1&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyCggBEC4YsQMYgAQyCggCEC4YsQMYgAQyDAgDECMYJxiABBiKBTIKCAQQABixAxiABDIGCAUQRRg8MgYIBhBFGDwyBggHEEUYPNIBBzY1OWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8"
+  );
 });
 
-
-test("Page Playwright test", async ({page}) => {
- 
-   await  page.goto("https://google.com");
-
+test("Page Playwright test", async ({ page }) => {
+  await page.goto("https://google.com");
 });
